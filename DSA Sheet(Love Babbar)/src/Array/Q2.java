76492b551f92c8e7311921ed09675442c451dff2
Shubham <shubhamkakade061@gmail.com>
2021-05-17 14:30:04 +0530

    Array Question Updated

--- DSA Sheet(Love Babbar)/src/Array/Q2.java
+++ DSA Sheet(Love Babbar)/src/Array/Q2.java
@@ -3,8 +3,22 @@ package Array;
 public class Q2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+		
+		int n;
+		int arr[]= {10,2,5,3,6,85,61,85};
+		
+		int max=0,min=arr[0];
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			if(arr[i]>max)
+				max=arr[i];
+			else if(arr[i]<min)
+				min=arr[i];
+		}
+		System.out.println("Maximum :"+max);
+		System.out.println("Minimum :"+min);
+		
+		}
 
 }
